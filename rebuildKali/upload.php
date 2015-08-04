3967fb2b75e36ec1c8d9193019b0d23a9048d4cb
5il <5il@users.noreply.github.com>
2015-08-04 08:57:10 -0400

    Update upload.php

--- rebuildKali/upload.php
+++ rebuildKali/upload.php
@@ -3,7 +3,7 @@ $uploaddir = "/tmp/";
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 echo "<p>";
 if( move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile) ){
-	echo "File is valid, and was suvvessfully uploaded.\n";
+	echo "File is valid, and was successfully uploaded.\n";
 } else {
 	echo "Upload failed";
 }
