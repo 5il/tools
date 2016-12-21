1165af936d5233c17ada0086abd3cd55c0f792e0
5il <kmcgowa2@gmu.edu>
2016-12-21 10:39:21 -0500

    Moved rebuildKali to dedicated repo

--- rebuildKali/index.php
+++
@@ -1,5 +0,0 @@
-<form enctype="multipart/form-data" action="upload.php" method="POST">
-<input type="hidden" name="MAX_FILE_SIZE" value="134217728" />
-Send this file: <input name="userfile" type="file" />
-<input type="submit" value="Send File" />
-</form>
